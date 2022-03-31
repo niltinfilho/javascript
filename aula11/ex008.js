aad46f4351de4810aee8aa3061ffc9048db366b3
Nilton Filho <niltonfilho115@gmail.com>
2022-03-31 20:52:40 -0300

    aula 11

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('Olá, mundo!')8
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
