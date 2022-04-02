4aeda2273d2b35c0a6a432f91b6133fb63443116
Nilton Filho <niltonfilho115@gmail.com>
2022-04-02 15:10:53 -0300

    primeiro exercicio como site

---
+++ aula12ex/ex014/codigo/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = document.querySelector('div#msg')
+    var bom = document.querySelector('div#bom')
+    var img = document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = 4 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 && hora < 12) {
+        // BOM DIA!
+        img.scr = 'imagens/foto-manha.png'
+        document.body.style.background = '#c3b187'
+        bom.innerHTML = 'BOM DIA!'
+    } else if (  hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.background = '#b9846f'
+        bom.innerHTML = 'BOA TARDE!'
+    } else {
+        // BOA NOITE
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.background = '#3b3b3d'
+        bom.innerHTML = 'BOA NOITE!'
+    }
+}
