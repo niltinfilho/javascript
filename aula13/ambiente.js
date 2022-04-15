73272b891eef3d44b9f7492b21f03fcd04cb7be8
Nilton Filho <niltonfilho115@gmail.com>
2022-04-15 14:31:07 -0300

    finalizei o exercicio

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,22 @@
-console.log
+var cont = 1
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++ // cont = cont + 1
+}
+
+
+
+var cont = 1
+do {
+    console.log('Tudo bem?')
+    cont++
+} while (cont <= 6)
+
+
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
